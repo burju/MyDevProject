58ce8961c1f7e1d07ff60a438d1b0972009a4f06
burju <kanliska@gmail.com>
2021-03-06 10:27:38 -0600

    first comm

---
+++ src/code.java
@@ -0,0 +1,11 @@
+public class code {
+
+        public static void main(String[] args) throws InterruptedException {
+// write your code here
+            for (int i = 0; i <5 ; i++) {
+                System.out.println("Hello batch 8");
+                Thread.sleep(2000);
+            }
+        }
+    }
+
